9c9b5c28488f58f1a8c8a772f6433783fa2ad51a
Sunkumar4978 <sunkumar125@gmail.com>
2024-06-16 14:24:32 +0530

    oneway data binding

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  name: string = 'sun';
+  age: number = 25;
 
+  product = {
+    mname: 'oneplus',
+    price: '40000'
+  }
+
+  onChange() {
+    this.name = 'arun';
+  }
+
+  onChangeSecondExample(event: any) {
+    console.log('event', event);
+    // output => we get inputevent object - target - value 
+    console.log(event.target.value)
+    this.name = event.target.value;
+  }
+
+  addtoCart: number = 10;
+  stock: number = 15;
+  isAddToCartDisabled: boolean = false;
+  increment(event: any) {
+    if (this.addtoCart < this.stock) {
+      this.addtoCart++;
+      this.isAddToCartDisabled = this.addtoCart === this.stock;
+      this.isDecrementDisabled = false;
+    }
+  }
+  isDecrementDisabled: boolean = false;
+  decrement(event: any) {
+    if (this.addtoCart > 0) {
+      this.addtoCart--;
+      this.isDecrementDisabled = this.addtoCart === 0;
+      this.isAddToCartDisabled = false;
+    }
+  }
 }
